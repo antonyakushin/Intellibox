e485c9a6d9d78e913e0cf7ba0693fc6d7388528f
Joe Feser <joseph.feser@gmail.com>
2010-01-17 00:13:00 +0000

    Added support for CancelAllSearches as an ICommand WorkItemId=3523

---
+++ IntelliBox/DelegateCommand.cs
@@ -0,0 +1,264 @@
+﻿/*
+Aquired from WPF Model-View-ViewModel Toolkit 0.1 
+ 
+http://www.codeplex.com/wpf/Release/ProjectReleases.aspx?ReleaseId=14962
+ 
+Microsoft Public License (Ms-PL)
+
+This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+
+1. Definitions
+
+The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+
+A "contribution" is the original software, or any additions or changes to the software.
+
+A "contributor" is any person that distributes its contribution under this license.
+
+"Licensed patents" are a contributor's patent claims that read directly on its contribution.
+
+2. Grant of Rights
+
+(A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+
+(B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+
+3. Conditions and Limitations
+
+(A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+
+(B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+
+(C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+
+(D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+
+(E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace System.Windows.Controls {
+
+    /// <summary>
+    ///     This class allows delegating the commanding logic to methods passed as parameters,
+    ///     and enables a View to bind commands to objects that are not part of the element tree.
+    /// </summary>
+    internal class DelegateCommand : ICommand {
+
+        #region Constructors
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public DelegateCommand(Action executeMethod)
+            : this(executeMethod, null, false) {
+        }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+            : this(executeMethod, canExecuteMethod, false) {
+        }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod, bool isAutomaticRequeryDisabled) {
+            if (executeMethod == null) {
+                throw new ArgumentNullException("executeMethod");
+            }
+
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+            _isAutomaticRequeryDisabled = isAutomaticRequeryDisabled;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        ///     Method to determine if the command can be executed
+        /// </summary>
+        public bool CanExecute() {
+            if (_canExecuteMethod != null) {
+                return _canExecuteMethod();
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///     Execution of the command
+        /// </summary>
+        public void Execute() {
+            if (_executeMethod != null) {
+                _executeMethod();
+            }
+        }
+
+        /// <summary>
+        ///     Property to enable or disable CommandManager's automatic requery on this command
+        /// </summary>
+        public bool IsAutomaticRequeryDisabled {
+            get {
+                return _isAutomaticRequeryDisabled;
+            }
+            set {
+                if (_isAutomaticRequeryDisabled != value) {
+                    if (value) {
+                        CommandManagerHelper.RemoveHandlersFromRequerySuggested(_canExecuteChangedHandlers);
+                    }
+                    else {
+                        CommandManagerHelper.AddHandlersToRequerySuggested(_canExecuteChangedHandlers);
+                    }
+                    _isAutomaticRequeryDisabled = value;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Raises the CanExecuteChaged event
+        /// </summary>
+        public void RaiseCanExecuteChanged() {
+            OnCanExecuteChanged();
+        }
+
+        /// <summary>
+        ///     Protected virtual method to raise CanExecuteChanged event
+        /// </summary>
+        protected virtual void OnCanExecuteChanged() {
+            CommandManagerHelper.CallWeakReferenceHandlers(_canExecuteChangedHandlers);
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        /// <summary>
+        ///     ICommand.CanExecuteChanged implementation
+        /// </summary>
+        public event EventHandler CanExecuteChanged {
+            add {
+                if (!_isAutomaticRequeryDisabled) {
+                    CommandManager.RequerySuggested += value;
+                }
+                CommandManagerHelper.AddWeakReferenceHandler(ref _canExecuteChangedHandlers, value, 2);
+            }
+            remove {
+                if (!_isAutomaticRequeryDisabled) {
+                    CommandManager.RequerySuggested -= value;
+                }
+                CommandManagerHelper.RemoveWeakReferenceHandler(_canExecuteChangedHandlers, value);
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter) {
+            return CanExecute();
+        }
+
+        void ICommand.Execute(object parameter) {
+            Execute();
+        }
+
+        #endregion
+
+        #region Data
+
+        private readonly Action _executeMethod = null;
+        private readonly Func<bool> _canExecuteMethod = null;
+        private bool _isAutomaticRequeryDisabled = false;
+        private List<WeakReference> _canExecuteChangedHandlers;
+
+        #endregion
+    }
+
+
+    /// <summary>
+    ///     This class contains methods for the CommandManager that help avoid memory leaks by
+    ///     using weak references.
+    /// </summary>
+    internal class CommandManagerHelper {
+        internal static void CallWeakReferenceHandlers(List<WeakReference> handlers) {
+            if (handlers != null) {
+                // Take a snapshot of the handlers before we call out to them since the handlers
+                // could cause the array to me modified while we are reading it.
+
+                EventHandler[] callees = new EventHandler[handlers.Count];
+                int count = 0;
+
+                for (int i = handlers.Count - 1; i >= 0; i--) {
+                    WeakReference reference = handlers[i];
+                    EventHandler handler = reference.Target as EventHandler;
+                    if (handler == null) {
+                        // Clean up old handlers that have been collected
+                        handlers.RemoveAt(i);
+                    }
+                    else {
+                        callees[count] = handler;
+                        count++;
+                    }
+                }
+
+                // Call the handlers that we snapshotted
+                for (int i = 0; i < count; i++) {
+                    EventHandler handler = callees[i];
+                    handler(null, EventArgs.Empty);
+                }
+            }
+        }
+
+        internal static void AddHandlersToRequerySuggested(List<WeakReference> handlers) {
+            if (handlers != null) {
+                foreach (WeakReference handlerRef in handlers) {
+                    EventHandler handler = handlerRef.Target as EventHandler;
+                    if (handler != null) {
+                        CommandManager.RequerySuggested += handler;
+                    }
+                }
+            }
+        }
+
+        internal static void RemoveHandlersFromRequerySuggested(List<WeakReference> handlers) {
+            if (handlers != null) {
+                foreach (WeakReference handlerRef in handlers) {
+                    EventHandler handler = handlerRef.Target as EventHandler;
+                    if (handler != null) {
+                        CommandManager.RequerySuggested -= handler;
+                    }
+                }
+            }
+        }
+
+        internal static void AddWeakReferenceHandler(ref List<WeakReference> handlers, EventHandler handler) {
+            AddWeakReferenceHandler(ref handlers, handler, -1);
+        }
+
+        internal static void AddWeakReferenceHandler(ref List<WeakReference> handlers, EventHandler handler, int defaultListSize) {
+            if (handlers == null) {
+                handlers = (defaultListSize > 0 ? new List<WeakReference>(defaultListSize) : new List<WeakReference>());
+            }
+
+            handlers.Add(new WeakReference(handler));
+        }
+
+        internal static void RemoveWeakReferenceHandler(List<WeakReference> handlers, EventHandler handler) {
+            if (handlers != null) {
+                for (int i = handlers.Count - 1; i >= 0; i--) {
+                    WeakReference reference = handlers[i];
+                    EventHandler existingHandler = reference.Target as EventHandler;
+                    if ((existingHandler == null) || (existingHandler == handler)) {
+                        // Clean up old handlers that have been collected
+                        // in addition to the handler that is to be removed.
+                        handlers.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+}
