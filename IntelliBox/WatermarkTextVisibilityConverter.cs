5d5e235ec88fdc454aa0789895cbdf1f2f418830
StephenWard1984 <StephenWard1984@localhost>
2010-05-29 01:03:50 +0000

    finished coding for the WatermarkText functionality

---
+++ IntelliBox/WatermarkTextVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Text;
+
+namespace FeserWard.Controls {
+    /// <summary>
+    /// Takes as input multiple boolean parameters and ORs them together for the final result.
+    /// ( will AND them together if the parameter is 'invert' )
+    /// </summary>
+    public sealed class WatermarkTextVisibilityConverter : IMultiValueConverter {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (values == null || values.Length != 2)
+                return DependencyProperty.UnsetValue;
+
+            var text = values[0].ToString();
+            var hasFocus = (bool)values[1];
+            var hasText = !string.IsNullOrEmpty(text);
+
+            return (hasFocus || hasText)
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException("The boolean OR conversion is a one-way conversion");
+        }
+    }
+}
