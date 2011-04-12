11167c0a0f83ef07074b379897194727c41217b6
StephenWard1984 <StephenWard1984@localhost>
2011-04-12 02:53:52 +0000

    fixed Xml comment warning

--- IntelliBox/WatermarkTextVisibilityConverter.cs
+++ IntelliBox/WatermarkTextVisibilityConverter.cs
@@ -6,11 +6,22 @@ using System.Text;
 
 namespace FeserWard.Controls {
     /// <summary>
-    /// Takes as input multiple boolean parameters and ORs them together for the final result.
-    /// ( will AND them together if the parameter is 'invert' )
+    /// Determines whether to show or hide watermark text of a control based on
+    /// whether the control has input or focus.
     /// </summary>
     public sealed class WatermarkTextVisibilityConverter : IMultiValueConverter {
 
+        /// <summary>
+        /// Determines whether to show or hide watermark text based on whether the
+        /// input control has focus or has a value. The first value passed to this
+        /// multi-value converter is assumed to be the Text value of the input control,
+        /// and the second value whether or not the control has focus.
+        /// </summary>
+        /// <param name="values">The boolean values to combine</param>
+        /// <param name="targetType">The <see cref="System.Type"/> of the return value. Should be <see cref="System.Boolean"/>.</param>
+        /// <param name="parameter">Not used.</param>
+        /// <param name="culture">Ignored.</param>
+        /// <returns><see cref="Visibility.Collapsed"/> if either of the parameters is true, otherwise <see cref="Visibility.Visible"/>.</returns>
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
             if (values == null || values.Length != 2)
                 return DependencyProperty.UnsetValue;
@@ -24,8 +35,17 @@ namespace FeserWard.Controls {
                 : Visibility.Visible;
         }
 
+        /// <summary>
+        /// This function is not supported. The <see cref="WatermarkTextVisibilityConverter"/> is
+        /// a one-way conversion. Calling this function will throw a <see cref="NotImplementedException"/>.
+        /// </summary>
+        /// <param name="value">Not Used.</param>
+        /// <param name="targetTypes">Not Used.</param>
+        /// <param name="parameter">Not Used.</param>
+        /// <param name="culture">Not Used.</param>
+        /// <returns>Not Used.</returns>
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
-            throw new NotImplementedException("The boolean OR conversion is a one-way conversion");
+            throw new NotImplementedException("Cannot ConvertBack() a WatermarkTextVisibilityConverter.");
         }
     }
 }
