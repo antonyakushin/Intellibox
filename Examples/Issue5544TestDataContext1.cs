63e6870f469a27b6d45fa68be90ab3808be7e97b
Stephen Ward <spworwell@gmail.com>
2013-04-16 16:34:16 -0400

    created a test case for issue #5544

---
+++ Examples/Issue5544TestDataContext1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Examples
+{
+	class Issue5544TestDataContext1 : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			var e = PropertyChanged;
+			if (e != null)
+			{
+				var args = new PropertyChangedEventArgs(propertyName);
+				e.Invoke(this, args);
+			}
+		}
+
+		public string DataContextName { get { return "Data Context #1"; } }
+
+		private string _greeting;
+		public string greetingTxt {
+			get { return _greeting; }
+			set { if (value != _greeting) { _greeting = value; OnPropertyChanged("greetingTxt"); } } 
+		}
+
+		private string _name;
+		public string nameTxt
+		{
+			get { return _name; }
+			set { if (value != _name) { _name = value; OnPropertyChanged("nameTxt"); } }
+		}
+
+		private bool _spam;
+		public bool sendSpam
+		{
+			get { return _spam; }
+			set { if (value != _spam) { _spam = value; OnPropertyChanged("sendSpam"); } }
+		}
+
+		private FeserWard.Controls.IIntelliboxResultsProvider _results = new queryProvider();
+		public FeserWard.Controls.IIntelliboxResultsProvider queryResults { get { return _results; } }
+
+		private string _value;
+		public string valueSelected
+		{
+			get { return _value; }
+			set { if (value != _value) { _value = value; OnPropertyChanged("valueSelected"); } }
+		}
+
+		private class queryProvider : FeserWard.Controls.IIntelliboxResultsProvider
+		{
+
+			public System.Collections.IEnumerable DoSearch(string searchTerm, int maxResults, object extraInfo)
+			{
+				return new result[] {
+					new result { Name ="name 1-1", Salutation= "salutation 1-1"},
+					new result { Name ="name 1-2", Salutation= "salutation 1-2"},
+					new result { Name ="name 1-3", Salutation= "salutation 1-3"}
+				};
+			}
+
+			private class result
+			{
+				public string Name { get; set; }
+				public string Salutation { get; set; }
+			}
+		}
+	}
+}
---
+++ Examples/Issue5544TestDataContext2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Examples
+{
+	class Issue5544TestDataContext2 : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			var e = PropertyChanged;
+			if (e != null)
+			{
+				var args = new PropertyChangedEventArgs(propertyName);
+				e.Invoke(this, args);
+			}
+		}
+
+		public string DataContextName { get { return "Data Context #2"; } }
+
+		private string _greeting;
+		public string greetingTxt
+		{
+			get { return _greeting; }
+			set { if (value != _greeting) { _greeting = value; OnPropertyChanged("greetingTxt"); } }
+		}
+
+		private string _name;
+		public string nameTxt
+		{
+			get { return _name; }
+			set { if (value != _name) { _name = value; OnPropertyChanged("nameTxt"); } }
+		}
+
+		private bool _spam;
+		public bool sendSpam
+		{
+			get { return _spam; }
+			set { if (value != _spam) { _spam = value; OnPropertyChanged("sendSpam"); } }
+		}
+
+		private FeserWard.Controls.IIntelliboxResultsProvider _results = new queryProvider();
+		public FeserWard.Controls.IIntelliboxResultsProvider queryResults { get { return _results; } }
+
+		private string _value;
+		public string valueSelected
+		{
+			get { return _value; }
+			set { if (value != _value) { _value = value; OnPropertyChanged("valueSelected"); } }
+		}
+
+		private class queryProvider : FeserWard.Controls.IIntelliboxResultsProvider
+		{
+
+			public System.Collections.IEnumerable DoSearch(string searchTerm, int maxResults, object extraInfo)
+			{
+				return new testresult[] {
+					new testresult { Name ="name 2-1", Salutation= "salutation 2-1"},
+					new testresult { Name ="name 2-2", Salutation= "salutation 2-2"},
+					new testresult { Name ="name 2-3", Salutation= "salutation 2-3"}
+				};
+			}
+
+			private class testresult
+			{
+				public string Name { get; set; }
+				public string Salutation { get; set; }
+			}
+		}
+	}
+}
