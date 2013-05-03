42c950579bb27cbdefb1b5e988e44facba423fc5
Stephen Ward <spworwell@gmail.com>
2013-05-02 21:27:33 -0400

    workitem 3516: started converting the project to use a basic implementation of MVVM w/ the Caliburn.Micro MVVM framework

---
+++ Examples/ViewModels/TestsVM.cs
@@ -0,0 +1,57 @@
+﻿/*
+Copyright (c) 2010 Stephen P Ward and Joseph E Feser
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Examples.ViewModels
+{
+    class TestsVM
+    {
+        public StandardSearchVM EmpthPathTestVM { get; private set; }
+        public StandardSearchVM ValueConvertersTestVM { get; private set; }
+
+        public StandardSearchVM OneWayToSourceTestVM { get; private set; } //TODO need to combine this test tab into a single tab
+
+        public StandardSearchVM WorkItem4391TestVM { get; private set; }
+        public WorkItem5544VM WorkItem5544TestVM { get; private set; }
+
+        public StandardSearchVM WorkItem6309TestVM { get; private set; }
+        public WorkItem6036VM WorkItem6036TestVM { get; private set; }
+
+        public TestsVM()
+        {
+            EmpthPathTestVM = new StandardSearchVM(new SingleColumnResultsProvider());
+            ValueConvertersTestVM = new StandardSearchVM(new SingleColumnResultsProvider());
+            OneWayToSourceTestVM = new StandardSearchVM(new MultiColumnResultsProvider());
+            WorkItem4391TestVM = new StandardSearchVM(new ObjectListProvider());
+            WorkItem5544TestVM = new WorkItem5544VM();
+            WorkItem6309TestVM = new StandardSearchVM(new WideAndNarrowResultsProvider());
+            WorkItem6036TestVM = new WorkItem6036VM();
+
+        }
+    }
+}
---
+++ Examples/ViewModels/WorkItem5544VM.cs
@@ -0,0 +1,66 @@
+﻿/*
+Copyright (c) 2010 Stephen P Ward and Joseph E Feser
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace Examples.ViewModels
+{
+    class WorkItem5544VM : PropertyChangedBase
+    {
+        private StandardSearchVM _dataContext1;
+        private StandardSearchVM _dataContext2;
+        private StandardSearchVM _currentContext;
+
+        public StandardSearchVM CurrentDataContext { 
+            get { 
+                return _currentContext; 
+            } 
+            private set { 
+                if (value != _currentContext) { 
+                    _currentContext = value;
+                    NotifyOfPropertyChange(() => CurrentDataContext); 
+                } 
+            } 
+        }
+
+        public void SwapDataContexts()
+        {
+            CurrentDataContext = (CurrentDataContext == _dataContext1)
+                ? _dataContext2
+                : _dataContext1;
+        }
+
+        public WorkItem5544VM()
+        {
+            _dataContext1 = new StandardSearchVM(new SingleColumnResultsProvider());
+            _dataContext2 = new StandardSearchVM(new MultiColumnResultsProvider());
+
+            CurrentDataContext = _dataContext1;
+        }
+    }
+}
