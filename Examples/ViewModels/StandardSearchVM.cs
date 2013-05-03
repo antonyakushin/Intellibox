ef9125951cf6bf519557aa4e80d667206794ca7d
StephenWard1984 <stephen.ward1984@hotmail.com>
2013-05-02 21:27:33 -0400

    workitem 3516: started converting the project to use a basic implementation of MVVM w/ the Caliburn.Micro MVVM framework

---
+++ Examples/ViewModels/StandardSearchVM.cs
@@ -0,0 +1,53 @@
+﻿/*
+Copyright (c) 2010 Stephen P Ward and Joseph E Feser
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FeserWard.Controls;
+using Caliburn.Micro;
+
+namespace Examples.ViewModels
+{
+    class StandardSearchVM : PropertyChangedBase
+    {
+        private object _selectedItem;
+        public object SelectedItem { get { return _selectedItem; } set { if (value != _selectedItem) { _selectedItem = value; NotifyOfPropertyChange(() => SelectedItem); } } }
+
+        private object _selectedValue;
+        public object SelectedValue { get { return _selectedValue; } set { if (value != _selectedValue) { _selectedValue = value; NotifyOfPropertyChange(() => SelectedValue); } } }
+
+        private string _selectedText;
+        public string SelectedText { get { return _selectedText; } set { if (value != _selectedText) { _selectedText = value; NotifyOfPropertyChange(() => SelectedText); } } }
+
+        private IIntelliboxResultsProvider _queryProvider;
+        public IIntelliboxResultsProvider QueryProvider { get { return _queryProvider; } private set { if (value != _queryProvider) { _queryProvider = value; this.NotifyOfPropertyChange(() => QueryProvider); } } }
+
+        public StandardSearchVM(IIntelliboxResultsProvider provider)
+        {
+            QueryProvider = provider;
+        }
+    }
+}
